1ab25f275d1c9434de67c28370103fd8990d3b43
Dawa Tsheing <dawa.tshering@dcpl.bt>
2021-06-26 00:20:34 +0600

    profile

--- src/containers/UserManagement/Userprofile/UserProfile.js
+++ src/containers/UserManagement/Userprofile/UserProfile.js
@@ -1,15 +1,10 @@
-import React, {useContext, useState} from 'react';
+import React, {useState} from 'react';
 import {Grid, makeStyles, Paper, Typography} from '@material-ui/core';
-import Button from '@material-ui/core/Button';
 import {UserEventCreated} from './UserEventCreated';
-import {StateContext} from '../../../../store';
-import {fetchIncludedObject} from '../../../../shared/functions/IncludedBinding';
-import Moment from 'react-moment';
 import EditIcon from '@material-ui/icons/Edit';
 import IconButton from '@material-ui/core/IconButton';
 import {useHistory} from 'react-router-dom';
-import EditProfile from './EditProfile';
-import RenderAuthorized from '../../../../routes/RenderAuthorized';
+import {useTheme} from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -44,19 +39,13 @@ const useStyles = makeStyles(theme => ({
     fontSize: 18,
     marginBottom: 10
   },
-  registrationCard: {
-    padding: 10,
-    backgroundColor: '#c4c4c4',
-  }
 }));
 
 export const UserProfile = () => {
   const history = useHistory();
   const classes = useStyles();
-  const {user} = useContext(StateContext);
-  const [profile, setProfile] = useState(user);
+  const theme = useTheme();
   const [open, setOpen] = useState(false);
-  let relationship = profile?.data?.relationships?.profile_attributes?.data;
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -71,57 +60,32 @@ export const UserProfile = () => {
         <Paper className={classes.paper}>
           <Grid container>
             <Grid item lg={3} xs={6}>
-              <img src={require('../../../../assets/images/default.png').default} className={classes.image} alt=""/>
+              <img src={require('../../../assets/images/default.png').default} className={classes.image} alt=""/>
             </Grid>
             <Grid item lg={5} xs={6}>
               <Typography className={classes.name}>
-                {fetchIncludedObject(relationship, profile?.included)?.attributes?.first_name ?? 'Event'}
-                {' '}
-                {fetchIncludedObject(relationship, profile?.included)?.attributes?.last_name ?? 'User'}
+                Name
               </Typography>
               <Typography className={classes.detail}>
-                {profile?.data?.attributes?.phone || '--'}
+                12345678
               </Typography>
               <Typography className={classes.detail}>
-                <Moment format="DD-MM-YYYY">
-                  {fetchIncludedObject(relationship, profile?.included)?.attributes?.dob ?? new Date()}
-                </Moment>
-              </Typography>
-              <Typography className={classes.detail}>
-                <a href={`mailto: ${profile?.data?.attributes?.email}`} target="_blank" rel="noreferrer noopener">
-                  {profile?.data?.attributes?.email ?? '--'}
+                <a href='mailto: kinga@gmail.com' target="_blank" rel="noreferrer noopener">
+                  dawa@gmail.com
                 </a>
               </Typography>
             </Grid>
-            <RenderAuthorized authorized={['Admin']}>
-              <Grid item lg={4} xs={6} align="right">
-                <Button className={classes.button}>Deactivate</Button>
-              </Grid>
-            </RenderAuthorized>
           </Grid>
           <Grid item lg={12} xs={12} align="right">
-            <IconButton style={{backgroundColor: 'blue'}}>
-              <EditIcon onClick={() => handleClickOpen()}/>
+            <IconButton style={{backgroundColor: theme.primary}}>
+              <EditIcon style={{color: 'white'}} onClick={() => handleClickOpen()}/>
             </IconButton>
           </Grid>
-          <Typography className={classes.registration}>
-            Registration Date
-          </Typography>
-          <Paper className={classes.registrationCard}>
-            <Typography>
-              <Moment format="DD-MM-YYYY">
-                {profile?.data?.attributes?.created_at}
-              </Moment>
-            </Typography>
-          </Paper>
         </Paper>
       </Grid>
-      <EditProfile handleClose={handleClose} open={open} editUser={profile} relations={relationship}/>
       <Grid item lg={7} xs={12}>
-        <Paper className="p-3">
-          <Typography style={{fontWeight: 700, textAlign: 'center', marginBottom: 10}}>Events Created</Typography>
-          <UserEventCreated/>
-        </Paper>
+        <Typography style={{fontWeight: 700, textAlign: 'center', marginBottom: 10}}>Courses Created</Typography>
+        <UserEventCreated/>
       </Grid>
     </Grid>
   );

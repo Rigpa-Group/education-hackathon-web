d4b02bac38cf144141fdbaf64420220113cb0ebb
Dawa Tsheing <dawa.tshering@dcpl.bt>
2021-06-26 18:16:14 +0600

    Course list

--- src/containers/Courses/ListCourse.js
+++ src/containers/Courses/ListCourse.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ViewAll from '../../views/landing/view-all/ViewAll';
-import {makeStyles, Button} from '@material-ui/core';
+import {makeStyles, Button, Table, TableHead, TableRow, TableCell, TableBody, TableContainer} from '@material-ui/core';
 import {useHistory} from 'react-router-dom'
 import RenderAuthorized from '../../routes/RenderAuthorized';
+import TablePagination from '@material-ui/core/TablePagination';
+import {useTheme} from '@material-ui/core';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const useStyles = makeStyles(theme => ({
   button: {
@@ -18,6 +22,18 @@ const useStyles = makeStyles(theme => ({
 export const ListCourse = () => {
   const classes = useStyles();
   const history = useHistory();
+  const theme = useTheme();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   const addCourse = () => {
     history.push('/course/add')
@@ -30,7 +46,88 @@ export const ListCourse = () => {
         <Button variant="contained" className={classes.button} onClick={addCourse}>Add Course</Button>
       </section>
       </RenderAuthorized>
-      <ViewAll/>
+      <TableContainer>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell style={{color: theme.primary, fontSize: 18}}>Sl.no</TableCell>
+              <TableCell style={{color: theme.primary, fontSize: 18}}>Course Name</TableCell>
+              <TableCell style={{color: theme.primary, fontSize: 18}}>Course Category</TableCell>
+              <TableCell style={{color: theme.primary, fontSize: 18}}>Tutor</TableCell>
+              <TableCell style={{color: theme.primary, fontSize: 18}}>Posted Date</TableCell>
+              <TableCell style={{color: theme.primary, fontSize: 18}}>Status</TableCell>
+              <TableCell/>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell style={{color: '#727070', fontSize: 14}}>1</TableCell>
+              <TableCell style={{color: '#727070', fontSize: 14}}>Hacking Tutorials</TableCell>
+              <TableCell style={{color: '#727070', fontSize: 14}}>Information Technology</TableCell>
+              <TableCell style={{color: '#727070', fontSize: 14}}>Zala Kinga Norbu</TableCell>
+              <TableCell style={{color: '#727070', fontSize: 14}}>12/12/2022</TableCell>
+              <TableCell style={{color: '#727070', fontSize: 14}}>Approved</TableCell>
+              <TableCell>
+                <Tooltip title="Detail">
+                  <ArrowForwardIcon style={{color: theme.primary}}/>
+                </Tooltip>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell style={{color: '#727070', fontSize: 14}}>1</TableCell>
+              <TableCell style={{color: '#727070', fontSize: 14}}>Hacking Tutorials</TableCell>
+              <TableCell style={{color: '#727070', fontSize: 14}}>Information Technology</TableCell>
+              <TableCell style={{color: '#727070', fontSize: 14}}>Zala Kinga Norbu</TableCell>
+              <TableCell style={{color: '#727070', fontSize: 14}}>12/12/2022</TableCell>
+              <TableCell style={{color: '#727070', fontSize: 14}}>Approved</TableCell>
+              <TableCell>
+                <Tooltip title="Detail">
+                  <ArrowForwardIcon style={{color: theme.primary}}/>
+                </Tooltip>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell style={{color: '#727070', fontSize: 14}}>1</TableCell>
+              <TableCell style={{color: '#727070', fontSize: 14}}>Hacking Tutorials</TableCell>
+              <TableCell style={{color: '#727070', fontSize: 14}}>Information Technology</TableCell>
+              <TableCell style={{color: '#727070', fontSize: 14}}>Zala Kinga Norbu</TableCell>
+              <TableCell style={{color: '#727070', fontSize: 14}}>12/12/2022</TableCell>
+              <TableCell style={{color: '#727070', fontSize: 14}}>Approved</TableCell>
+              <TableCell>
+                <Tooltip title="Detail">
+                  <ArrowForwardIcon style={{color: theme.primary}}/>
+                </Tooltip>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell style={{color: '#727070', fontSize: 14}}>1</TableCell>
+              <TableCell style={{color: '#727070', fontSize: 14}}>Hacking Tutorials</TableCell>
+              <TableCell style={{color: '#727070', fontSize: 14}}>Information Technology</TableCell>
+              <TableCell style={{color: '#727070', fontSize: 14}}>Zala Kinga Norbu</TableCell>
+              <TableCell style={{color: '#727070', fontSize: 14}}>12/12/2022</TableCell>
+              <TableCell style={{color: '#727070', fontSize: 14}}>Approved</TableCell>
+              <TableCell>
+                <Tooltip title="Detail">
+                  <ArrowForwardIcon style={{color: theme.primary}}/>
+                </Tooltip>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        <TablePagination
+          style={{float: 'right'}}
+          rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+          colSpan={3}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          SelectProps={{
+            inputProps: { 'aria-label': 'rows per page' },
+            native: true,
+          }}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </TableContainer>
     </React.Fragment>
   )
 }
